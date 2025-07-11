03412acb4b4a56ed15b1d8d33943ace7d0ada925
Devraj Katkoria <dk721@ic.ac.uk>
2025-07-11 18:02:35 +0100

    Code: Engine Mod 2: Basic Chess Theory

--- include/engine.hpp
+++ include/engine.hpp
@@ -14,6 +14,24 @@ struct SearchResult {
     int nodes;
 };
 
+// Tunable parameters controlling the evaluation function.  They are kept
+// outside of the evaluate() call so tests or front-ends can tweak the engine
+// without recompiling.
+struct EvalParams {
+    int mobilityWeight      = 2;   // weighting for simple mobility term
+    int spaceControlWeight  = 10;  // bonus per extra controlled square
+    int imbalanceWeight     = 30;  // penalty for pieces that are under defended
+    int outpostKnightBonus  = 25;  // reward for each protected outpost
+    int openFileBonus       = 10;  // rook on open file bonus
+    int seventhRankBonus    = 20;  // rook on the 7th rank bonus
+    int pawnTensionBonus    = 5;   // bonus per pawn tension
+    int pawnBreakBonus      = 10;  // bonus per available pawn break
+    int initiativeWeight    = 5;   // side to move forcing move bonus
+    int kingSafetyWeight    = 5;   // bonus for king flight squares
+};
+
+extern EvalParams evalParams;
+
 int evaluate(const Board &b);
 
 SearchResult search(Board &board, int maxDepth);
--- src/engine.cpp
+++ src/engine.cpp
@@ -8,6 +8,8 @@ namespace Engine {
 
 static const int INF = 100000;
 
+EvalParams evalParams;
+
 // Piece values
 static const int pieceValue[6] = {
     100,   // pawn
@@ -101,6 +103,70 @@ static int pst_value(PieceType pt, int sq, Color c){
     else return (*pst[pt-1])[63 - sq];
 }
 
+// count how many pieces of the given color attack a particular square
+static int attack_count(const Board &b, int sq, Color c){
+    int cnt = 0;
+    U64 occ = b.bothOccupancy;
+    U64 bb;
+    // pawns
+    bb = b.bitboards[board_index(c, PAWN)];
+    while(bb){
+        int from = pop_lsb(bb);
+        if(pawnAttacks[c==WHITE?0:1][from] & (1ULL<<sq)) cnt++;
+    }
+    // knights
+    bb = b.bitboards[board_index(c, KNIGHT)];
+    while(bb){
+        int from = pop_lsb(bb);
+        if(knightAttacks[from] & (1ULL<<sq)) cnt++;
+    }
+    // bishops
+    bb = b.bitboards[board_index(c, BISHOP)];
+    while(bb){
+        int from = pop_lsb(bb);
+        if(bishop_attacks(from, occ) & (1ULL<<sq)) cnt++;
+    }
+    // rooks
+    bb = b.bitboards[board_index(c, ROOK)];
+    while(bb){
+        int from = pop_lsb(bb);
+        if(rook_attacks(from, occ) & (1ULL<<sq)) cnt++;
+    }
+    // queens
+    bb = b.bitboards[board_index(c, QUEEN)];
+    while(bb){
+        int from = pop_lsb(bb);
+        if(queen_attacks(from, occ) & (1ULL<<sq)) cnt++;
+    }
+    // king
+    bb = b.bitboards[board_index(c, KING)];
+    if(bb){
+        int from = __builtin_ctzll(bb);
+        if(kingAttacks[from] & (1ULL<<sq)) cnt++;
+    }
+    return cnt;
+}
+
+// compute the attack bitboard for a side
+static U64 attacks_for_side(const Board &b, Color c){
+    U64 occ = b.bothOccupancy;
+    U64 att = 0ULL;
+    U64 bb;
+    bb = b.bitboards[board_index(c, PAWN)];
+    while(bb){ int from = pop_lsb(bb); att |= pawnAttacks[c==WHITE?0:1][from]; }
+    bb = b.bitboards[board_index(c, KNIGHT)];
+    while(bb){ int from = pop_lsb(bb); att |= knightAttacks[from]; }
+    bb = b.bitboards[board_index(c, BISHOP)];
+    while(bb){ int from = pop_lsb(bb); att |= bishop_attacks(from,occ); }
+    bb = b.bitboards[board_index(c, ROOK)];
+    while(bb){ int from = pop_lsb(bb); att |= rook_attacks(from,occ); }
+    bb = b.bitboards[board_index(c, QUEEN)];
+    while(bb){ int from = pop_lsb(bb); att |= queen_attacks(from,occ); }
+    bb = b.bitboards[board_index(c, KING)];
+    if(bb){ int from = __builtin_ctzll(bb); att |= kingAttacks[from]; }
+    return att;
+}
+
 int evaluate(const Board &b){
     int score = 0;
 
@@ -143,7 +209,7 @@ int evaluate(const Board &b){
     tmp = b;
     tmp.sideToMove = BLACK;
     int blackMoves = generate_legal_moves(tmp).size();
-    score += 2 * (whiteMoves - blackMoves);
+    score += evalParams.mobilityWeight * (whiteMoves - blackMoves);
 
     // Central control: pieces occupying or attacking the center squares are
     // rewarded.  The four central squares are d4, e4, d5 and e5.
@@ -157,6 +223,158 @@ int evaluate(const Board &b){
         if(b.is_square_attacked(sq, WHITE)) score += 3;
         if(b.is_square_attacked(sq, BLACK)) score -= 3;
     }
+
+    // Square control: count attacked squares on the enemy side of the board
+    U64 whiteAtt = attacks_for_side(b, WHITE);
+    U64 blackAtt = attacks_for_side(b, BLACK);
+    U64 enemyHalfWhite = 0xFFFFFFFF00000000ULL; // ranks 5-8
+    U64 enemyHalfBlack = 0x00000000FFFFFFFFULL; // ranks 1-4
+    int whiteControl = __builtin_popcountll(whiteAtt & enemyHalfWhite);
+    int blackControl = __builtin_popcountll(blackAtt & enemyHalfBlack);
+    int wKingSq = b.king_square(BLACK);
+    if(wKingSq != -1)
+        whiteControl += __builtin_popcountll(whiteAtt & kingAttacks[wKingSq]);
+    int bKingSq = b.king_square(WHITE);
+    if(bKingSq != -1)
+        blackControl += __builtin_popcountll(blackAtt & kingAttacks[bKingSq]);
+    score += evalParams.spaceControlWeight * (whiteControl - blackControl);
+
+    // Attack vs defence imbalance
+    for(Color c : {WHITE,BLACK}){
+        Color them = (Color)(-c);
+        int sign = (c==WHITE)?-1:1; // penalty for the side being attacked
+        for(int pt=PAWN; pt<=KING; ++pt){
+            U64 bb2 = b.bitboards[board_index(c,(PieceType)pt)];
+            while(bb2){
+                int sq = pop_lsb(bb2);
+                int att = attack_count(b,sq,them);
+                int def = attack_count(b,sq,c);
+                if(att>def)
+                    score += sign * evalParams.imbalanceWeight * (att-def);
+            }
+        }
+    }
+
+    // Outposts for knights in the enemy half not attackable by enemy pawns
+    int whiteOutposts = 0, blackOutposts = 0;
+    U64 whiteKnights = b.bitboards[board_index(WHITE, KNIGHT)];
+    while(whiteKnights){
+        int sq = pop_lsb(whiteKnights);
+        if(sq >= 32){
+            U64 enemyPawns = b.bitboards[board_index(BLACK, PAWN)];
+            bool attacked = false;
+            while(enemyPawns){
+                int psq = pop_lsb(enemyPawns);
+                if(pawnAttacks[1][psq] & (1ULL<<sq)) { attacked=true; break; }
+            }
+            if(!attacked) whiteOutposts++;
+        }
+    }
+    U64 blackKnights = b.bitboards[board_index(BLACK, KNIGHT)];
+    while(blackKnights){
+        int sq = pop_lsb(blackKnights);
+        if(sq < 32){
+            U64 enemyPawns = b.bitboards[board_index(WHITE, PAWN)];
+            bool attacked = false;
+            while(enemyPawns){
+                int psq = pop_lsb(enemyPawns);
+                if(pawnAttacks[0][psq] & (1ULL<<sq)) { attacked=true; break; }
+            }
+            if(!attacked) blackOutposts++;
+        }
+    }
+    score += evalParams.outpostKnightBonus * (whiteOutposts - blackOutposts);
+
+    // Rook activity on open or semi-open files and on the seventh rank
+    int rookOpenDiff = 0;
+    int rook7thDiff = 0;
+    U64 allPawns = b.bitboards[board_index(WHITE,PAWN)] |
+                   b.bitboards[board_index(BLACK,PAWN)];
+    for(Color c : {WHITE,BLACK}){
+        int sign = (c==WHITE)?1:-1;
+        U64 rooks = b.bitboards[board_index(c,ROOK)];
+        while(rooks){
+            int sq = pop_lsb(rooks);
+            int file = sq % 8;
+            U64 fileMask = 0x0101010101010101ULL << file;
+            bool ourPawn = b.bitboards[board_index(c,PAWN)] & fileMask;
+            bool oppPawn = b.bitboards[board_index((Color)-c,PAWN)] & fileMask;
+            if(!ourPawn && !oppPawn) rookOpenDiff += sign*2;
+            else if(!ourPawn) rookOpenDiff += sign;
+            int rank = sq/8;
+            if((c==WHITE && rank==6) || (c==BLACK && rank==1))
+                rook7thDiff += sign;
+        }
+    }
+    score += evalParams.openFileBonus * rookOpenDiff;
+    score += evalParams.seventhRankBonus * rook7thDiff;
+
+    // Pawn tension: pawns facing each other
+    int whiteTension=0, blackTension=0;
+    U64 wPawns = b.bitboards[board_index(WHITE, PAWN)];
+    while(wPawns){
+        int sq = pop_lsb(wPawns);
+        if(sq+8 < 64 && (b.bitboards[board_index(BLACK, PAWN)] & (1ULL<<(sq+8))))
+            whiteTension++;
+    }
+    U64 bPawns = b.bitboards[board_index(BLACK, PAWN)];
+    while(bPawns){
+        int sq = pop_lsb(bPawns);
+        if(sq-8 >=0 && (b.bitboards[board_index(WHITE, PAWN)] & (1ULL<<(sq-8))))
+            blackTension++;
+    }
+    score += evalParams.pawnTensionBonus * (whiteTension - blackTension);
+
+    // Pawn breaks: available pawn captures or double pushes
+    tmp = b; tmp.sideToMove = WHITE;
+    auto wm = generate_legal_moves(tmp);
+    int wBreaks=0;
+    for(const auto &m: wm){
+        if(m.piece==PAWN && (m.captured!=NO_PIECE || m.isDoublePush))
+            wBreaks++;
+    }
+    tmp = b; tmp.sideToMove = BLACK;
+    auto bm = generate_legal_moves(tmp);
+    int bBreaks=0;
+    for(const auto &m: bm){
+        if(m.piece==PAWN && (m.captured!=NO_PIECE || m.isDoublePush))
+            bBreaks++;
+    }
+    score += evalParams.pawnBreakBonus * (wBreaks - bBreaks);
+
+    // Initiative: forcing moves for each side
+    int wForcing=0, bForcing=0;
+    for(const auto &m: wm){
+        if(m.captured!=NO_PIECE){ wForcing++; continue; }
+        Board cb = b; Undo u = make_move(cb,m);
+        int ksq2 = cb.king_square(BLACK);
+        if(ksq2!=-1 && cb.is_square_attacked(ksq2,WHITE)) wForcing++;
+        undo_move(cb,m,u);
+    }
+    for(const auto &m: bm){
+        if(m.captured!=NO_PIECE){ bForcing++; continue; }
+        Board cb = b; Undo u = make_move(cb,m);
+        int ksq2 = cb.king_square(WHITE);
+        if(ksq2!=-1 && cb.is_square_attacked(ksq2,BLACK)) bForcing++;
+        undo_move(cb,m,u);
+    }
+    score += evalParams.initiativeWeight * (wForcing - bForcing);
+
+    // King safety: count safe flight squares around each king
+    int wSafe=0, bSafe=0;
+    if(bKing != -1){
+        U64 area = kingAttacks[bKing];
+        U64 mask = area & ~b.blackOccupancy;
+        mask &= ~whiteAtt;
+        bSafe = __builtin_popcountll(mask);
+    }
+    if(wKing != -1){
+        U64 area = kingAttacks[wKing];
+        U64 mask = area & ~b.whiteOccupancy;
+        mask &= ~blackAtt;
+        wSafe = __builtin_popcountll(mask);
+    }
+    score += evalParams.kingSafetyWeight * (wSafe - bSafe);
 
     return score;
 }
